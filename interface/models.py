79aec0dbee1004abefaef1728132c3d36d7b1aa9
Jason Zhu <jasonzhu503@gmail.com>
2019-01-13 08:48:54 -0500

    Improved homepage UI, implemented user base

--- interface/models.py
+++ interface/models.py
@@ -5,11 +5,11 @@ from django.contrib.auth.models import User
 class Challenge(models.Model):
 	title = models.CharField(max_length=100)
 	# Icon
-		# icon = models
-	# Progress Bar
-		# progress = models
+	icon_url = models.ImageField(default="default.jpg", upload_to="icons")
+	# Progress Bar in %
+	progress = models.IntegerField(default=0)
 	description = models.TextField()
-	deadline = models.DateTimeField(default=timezone.now)
+	deadline = models.DateField()
 
 	def __str__(self):
 		return self.title
--- interface/views.py
+++ interface/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render, redirect
 from .models import Challenge
 
 def home(request):
-	return render(request, 'interface/home.html', {'title': ''})
+	return render(request, 'interface/home.html', {'title': 'Home'})
 
 def progress(request):
 	context = {
+		'title': 'Progress',
 		'challenges': Challenge.objects.all()
 	}
 	return render(request, 'interface/progress.html', context)

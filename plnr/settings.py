e082c3b25262225e2d53633592d5bc1db8c7b1d8
jasonZJZhu <36906507+jasonZJZhu@users.noreply.github.com>
2019-02-04 00:38:30 -0500

    Update settings.py
    optimized settings

--- plnr/settings.py
+++ plnr/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=fwr58-32=0%^3_h0%65l^bgc=59u3muro#8l2df11cnsqy(l7'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
